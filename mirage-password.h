f7af86df2d073da4a218ed1db72c307d61b5e493
Michał Górny <mgorny@gentoo.org>
2010-12-07 17:35:12 +0100

    Add a missing include.

--- mirage-password.h
+++ mirage-password.h
@@ -6,6 +6,8 @@
 #ifndef _MIRAGE_PASSWORD_H
 #define _MIRAGE_PASSWORD_H 1
 
+#include <stdbool.h>
+
 const char* mirage_input_password(void);
 void mirage_forget_password(void);
 bool mirage_set_password(const char* const pass);
